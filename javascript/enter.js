408bb801bac4337b3d94b766612b8e77796bd238
professeur <awaloumassaoudou@gmail.com>
2023-12-01 22:37:58 +0100

    commit

--- javascript/enter.js
+++ javascript/enter.js
@@ -1,6 +1,18 @@
 //Changement du Curseur
 const cursor = document.querySelector(".cursor");
 document.addEventListener("mousemove", function(e){
-    console.log(e);
-    cursor.style.cssText = "left: " + e.clientX + "px; top:" + e.clientY + "px;" + e.bubbles + "true" ;  
+    cursor.style.cssText = "left: " + e.clientX + "px; top:" + e.clientY + "px;"  ;  
 })
+//___________________FIN_____________________
+//Cliquer de l'écran pour rentrer dans le site
+const ecran = document.querySelector(".ecran");
+const cliquerEcran = document.querySelector(".cliquer_ecran");
+const gifEcran = document.querySelector(".gif_ecran");
+cliquerEcran.addEventListener("click", () => {
+    cliquerEcran.style.display = "none";
+    gifEcran.classList.add("active");
+    setTimeout(() => {
+        location.href = "home.html";
+    }, 9000);
+})
+//___________________FIN________________________
